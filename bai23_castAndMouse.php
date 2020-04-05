d78a002f01ea4e6496ec2f7794d96fabe90a2c66
Dung PHUNG DUY <dphungduy@gmail.com>
2020-04-05 22:13:10 +0700

    catsAndMouse

---
+++ bai23_castAndMouse.php
@@ -0,0 +1,18 @@
+<?php
+//$q =2;
+$xyz1 = [1,2,3];
+//$xyz2 = [1,3,2];
+
+for($i=0;$i<3;$i++){
+    if($z-$x>$z-$y){
+        return "Cat B";
+    }
+    if($z-$x<$z-$y){
+        return "Cat A";
+    }
+ return "Mouse C";
+}
+
+
+
+?>
